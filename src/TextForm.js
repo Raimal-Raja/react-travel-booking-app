d3edfd5bf2b1e8a63d860c94c8d7612fe9e099c3
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-31 12:10:51 +0500

    initial commit

--- src/TextForm.js
+++ src/TextForm.js
@@ -8,6 +8,13 @@ export default function TextForom(props) {
         let newText = Text.toUpperCase();
         setText(newText)
     }
+    const handleLowerCase = () => {
+
+        console.log("Up was Lowercase was clicked!")
+        setText("You have clicked on Lowercase!")
+        let newText = Text.toLowerCase();
+        setText(newText)
+    }
 
     const handleOnChange = (event) => {
         console.log("Uppercase was clicked!")
@@ -18,13 +25,23 @@ export default function TextForom(props) {
     const [Text, setText] = useState("Enter text here! ")
     return (
         <>
-            <h1>{props.heading}</h1>
-       <div>
-            <div className="mb-3">
-                <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                <textarea className="form-control" value = {Text} id="exampleFormControlTextarea1" onChange = {handleOnChange} rows="3"></textarea>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div>
+                    <div className="mb-3">
+                        <textarea className="form-control" value={Text} id="exampleFormControlTextarea1" onChange={handleOnChange} rows="3"></textarea>
+                    </div>
+                    <button className='btn btn-primary mx-3' onClick={handleUpperCase}>Convert text to Uppercase</button>
+                    <button className='btn btn-primary' onClick={handleLowerCase}>Convert text to lowercase</button>
+                </div>
             </div>
-            <button className='btn btn-primary' onClick={handleUpperCase}>Convert text to Uppercase</button>
+            <div className="container my-4">
+                <h3>Your Text Summary</h3>
+                <p>{Text.split(" ").length} words and {Text.length} characters</p>
+                <p>{0.0008 * Text.split(" ").length} Mintues read</p>
+
+                <h3>Preview</h3>
+                <p>{Text}</p>
             </div>
         </>
     )
